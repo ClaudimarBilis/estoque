859a1bbcae605a24f1a42e77ded1832437c15a5c
ClaudimarBilis <claudimarbilisdev@gmail.com>
2026-09-02 09:55:29 -0300

    Criação da entidade Fornecedor com os campos básicos

---
+++ src/main/java/br/com/estudo/estoque/model/Fornecedor.java
@@ -0,0 +1,48 @@
+package br.com.estudo.estoque.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Fornecedor{
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+
+    public Long getId(){
+        return id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public Fornecedor(){
+    }
+
+    public Fornecedor(String nome, String email, String telefone){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+    }
+}
